fd6aa65dacecabef821b5db3f0020aa757295e7f
amirholya01 <amirholya01@gmail.com>
2024-02-17 19:27:07 +0100

    first commit

---
+++ src/test/java/com/amir/video_uploading/VideoUploadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amir.video_uploading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoUploadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
